243eb52bf9b82c5f4c38fcebd703c8a37900bc2c
Calder Dodd Russell <26crussell@cpsd.us>
2026-01-13 21:51:42 -0500

    potential fix for the reloading issues

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for assets
+  base: '/', // Use relative paths for assets
   build: {
     outDir: '../docs', // Build to the docs folder in the project root
     emptyOutDir: true, // Clear the directory before building
